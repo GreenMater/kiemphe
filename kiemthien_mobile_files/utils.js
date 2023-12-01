c89a7c805590c269fa9173743b2d5fd370a3698a
Dung.Nguyen <dung.nguyen@bossrotorcompany.com>
2023-12-01 17:35:32 +0700

    Update news, download... page

--- kiemthien_mobile_files/utils.js
+++ kiemthien_mobile_files/utils.js
@@ -1,3 +1,16 @@
 function openInNewTab(url) {
   window.open(url, '_blank').focus();
 }
+
+$(function() {
+    $('#toggle-right-menu').on('click', function() {
+        // $('#home-right-menu').slideToggle();
+        $('#home-right-menu').toggleClass('hide-by-slide-right', 'stick-to-right');
+        // flip << arrow
+        if ($('#home-right-menu').hasClass('hide-by-slide-right')) {
+            $("#toggle-right-menu").attr('style', "background-image: url('./kiemthien_mobile_files/media/img/_sprites-aside-3.png'); background-position: -80px -167px;");
+        } else {
+            $("#toggle-right-menu").attr('style', "background-image: url('./kiemthien_mobile_files/media/img/_sprites-aside-3.png'); background-position: -110px -167px;");
+        }
+    });
+});
